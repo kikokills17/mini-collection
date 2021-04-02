1485121f6775fec72b50c5536b0e3e30cd2c1a4f
Kiko Guan <70984203+kikokills17@users.noreply.github.com>
2021-04-02 20:52:07 +0800

    revised

--- script.js
+++ script.js
@@ -1,106 +1,8 @@
 let left = document.getElementsByClassName('left')[0];
-  let right = document.getElementsByClassName('right')[0];
-  let middle = document.getElementsByClassName('middle')[0];
-  let albums = [left, middle, right];
-
-  function init() {
-    for (let i = 0; i < albums.length; i++) {
-      let album = document.createElement('div');
-      album.classList.add('album-info');
-      albums[i].appendChild(album);
-      let img = makeImg(i);
-      let titles = makeTitles(i);
-      album.appendChild(img);
-      album.appendChild(titles);
-      album.classList.add('hidden');
-    }
-  }
-
-  function makeImg(index) {
-    let node = document.createElement('img');
-    if (index === 0) {
-      node.src = "./assets/highest.png";
-      return node;
-    } else if (index === 1) {
-      node.src = "./assets/711.png";
-      return node;
-    } else {
-      node.src = "./assets/sunrise.png";
-      return node;
-    }
-  }
-
-  function makeTitles(index) {
-    let div = document.createElement('div');
-    div.classList.add('titles');
-    if (index === 0) {
-      for (let i = 0; i < 3; i++) {
-        let node = document.createElement('h3');
-        if (i === 0) {
-          node.innerHTML = "<a href='https://music.apple.com/cn/album/highest-in-the-room-single/1481874613'>Highest In The Room</a>";
-        } else if (i === 1) {
-          node.innerHTML = "<a href='https://music.apple.com/cn/album/highest-in-the-room-single/1481874613'>Travis Scott</a>";
-        } else {
-          node.innerHTML = "<a href='https://music.apple.com/cn/album/highest-in-the-room-single/1481874613'>Highest In The Room</a>";
-        }
-        div.appendChild(node);
-      }
-    } else if (index === 1) {
-      for (let i = 0; i < 3; i++) {
-        let node = document.createElement('h3');
-        if (i === 0) {
-          node.innerHTML = "<a href='https://music.apple.com/cn/album/711/1243454429?i=1243455235'>711</a>";
-        } else if (i === 1) {
-          node.innerHTML = "<a href='https://music.apple.com/cn/album/711/1243454429?i=1243455235'>Higher Brother</a>";
-        } else {
-          node.innerHTML = "<a href='https://music.apple.com/cn/album/711/1243454429?i=1243455235'>Black Cab</a>";
-        }
-        div.appendChild(node);
-      }
-    } else {
-      for (let i = 0; i < 3; i++) {
-        let node = document.createElement('h3');
-        if (i === 0) {
-          node.innerHTML = "<a href='https://music.apple.com/cn/album/tequila-sunrise-feat-august-08-goldlink/1478261748?i=1478262069'>Tequila Sunrise</a>";
-        } else if (i === 1) {
-          node.innerHTML = "<a href='https://music.apple.com/cn/album/tequila-sunrise-feat-august-08-goldlink/1478261748?i=1478262069'>88Rising</a>";
-        } else {
-          node.innerHTML = "<a href='https://music.apple.com/cn/album/tequila-sunrise-feat-august-08-goldlink/1478261748?i=1478262069'>Head In The Clouds</a>";
-        }
-        div.appendChild(node);
-      }
-    }
-
-    return div;
-  }
-
-  init();
-
-  function hiddenInfo(index) {
-    document.getElementsByClassName('album-info')[index].classList.add('hidden');
-  }
-
-  function displayInfo(index) {
-    document.getElementsByClassName('album-info')[index].classList.remove('hidden');
-  }
-
-  for (let i = 0; i < albums.length; i++) {
-    albums[i].addEventListener('mouseover', function () {
-      (function () {
-        displayInfo(i);
-      })(i)
-    });
-    albums[i].addEventListener('mouseout', function () {
-      (function () {
-        hiddenInfo(i);
-      })(i)
-    });
-  }
-
-
-
-
-
+let right = document.getElementsByClassName('right')[0];
+let middle = document.getElementsByClassName('middle')[0];
+let albums = [left, middle, right];
+let infos = [];  // store data from airtable
 
 /* globals require */
 console.log("Hello, Airtable");
@@ -152,23 +54,118 @@ function consoleLogSongs() {
   });
 }
 
+
+// {
+//   name: "xxx",
+//   artist: "yyy",
+//   album: "zzz"
+//   url: "aaa"
+// }
+
 // loop through the books, create an h2 for each one, and add it to the page
 function showSongs() {
   console.log("showSongs()");
+  // get data
   songs.forEach((song) => {
-    var songName = document.createElement("h1");
-    songName.innerText = song.fields.name;
-    document.body.append(songName);
-
-    var nameOfArtist = document.createElement("h1");
-    nameOfArtist.innerText = song.fields.artist;
-    document.body.append(nameOfArtist);
-
-    var songImage = document.createElement("img");
-    songImage.src = song.fields.Attachments[0].url;
-    document.body.append(songImage);
+    infos.push({
+      name: song.fields.name,
+      artist: song.fields.artist,
+      album: song.fields.album,
+      url: song.fields.Attachments[0].url
+    });
   });
+  // then show them
+  init();
 }
 
+function init() {
+  for (let i = 0; i < albums.length; i++) {
+    let album = document.createElement('div');
+    album.classList.add('album-info');
+    albums[i].appendChild(album);
+    let img = makeImg(i);
+    let titles = makeTitles(i);
+    album.appendChild(img);
+    album.appendChild(titles);
+    album.classList.add('hidden');
+  }
+}
 
+function makeImg(index) {
+  let node = document.createElement('img');
+  if (index === 0) {
+    node.src = infos[0].url;
+    return node;
+  } else if (index === 1) {
+    node.src = infos[7].url;
+    return node;
+  } else {
+    node.src = infos[5].url;
+    return node;
+  }
+}
 
+function makeTitles(index) {
+  let div = document.createElement('div');
+  div.classList.add('titles');
+  if (index === 0) {
+    for (let i = 0; i < 3; i++) {
+      let node = document.createElement('h3');
+      if (i === 0) {
+        node.innerHTML = `<a href='https://music.apple.com/cn/album/highest-in-the-room-single/1481874613'>${infos[0].name}</a>`;
+      } else if (i === 1) {
+        node.innerHTML = `<a href='https://music.apple.com/cn/album/highest-in-the-room-single/1481874613'>${infos[0].artist}</a>`;
+      } else {
+        node.innerHTML = `<a href='https://music.apple.com/cn/album/highest-in-the-room-single/1481874613'>${infos[0].album}</a>`;
+      }
+      div.appendChild(node);
+    }
+  } else if (index === 1) {
+    for (let i = 0; i < 3; i++) {
+      let node = document.createElement('h3');
+      if (i === 0) {
+        node.innerHTML = `<a href='https://music.apple.com/cn/album/711/1243454429?i=1243455235'>${infos[7].name}</a>`;
+      } else if (i === 1) {
+        node.innerHTML = `<a href='https://music.apple.com/cn/album/711/1243454429?i=1243455235'>${infos[7].artist}</a>`;
+      } else {
+        node.innerHTML = `<a href='https://music.apple.com/cn/album/711/1243454429?i=1243455235'>${infos[7].album}</a>`;
+      }
+      div.appendChild(node);
+    }
+  } else {
+    for (let i = 0; i < 3; i++) {
+      let node = document.createElement('h3');
+      if (i === 0) {
+        node.innerHTML = `<a href='https://music.apple.com/cn/album/tequila-sunrise-feat-august-08-goldlink/1478261748?i=1478262069'>${infos[5].name}</a>`;
+      } else if (i === 1) {
+        node.innerHTML = `<a href='https://music.apple.com/cn/album/tequila-sunrise-feat-august-08-goldlink/1478261748?i=1478262069'>${infos[5].artist}</a>`;
+      } else {
+        node.innerHTML = `<a href='https://music.apple.com/cn/album/tequila-sunrise-feat-august-08-goldlink/1478261748?i=1478262069'>${infos[5].album}</a>`;
+      }
+      div.appendChild(node);
+    }
+  }
+
+  return div;
+}
+
+function hiddenInfo(index) {
+  document.getElementsByClassName('album-info')[index].classList.add('hidden');
+}
+
+function displayInfo(index) {
+  document.getElementsByClassName('album-info')[index].classList.remove('hidden');
+}
+
+for (let i = 0; i < albums.length; i++) {
+  albums[i].addEventListener('mouseover', function () {
+    (function () {
+      displayInfo(i);
+    })(i)
+  });
+  albums[i].addEventListener('mouseout', function () {
+    (function () {
+      hiddenInfo(i);
+    })(i)
+  });
+}
